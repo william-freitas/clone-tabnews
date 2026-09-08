3d483289a629823459156bea083cc5927e120ba1
william-freitas <119761554+william-freitas@users.noreply.github.com>
2026-09-08 19:33:15 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 repositório para o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Oi Lili meu amor ! Você é minha vida !</h1>
+function Home() {
+  return <h1>Oi Lili meu amor ! Você é minha vida !</h1>;
+}
+
+function teste2() {
+  console.log("identação errada corrigida com script de lint");
 }
 
 export default Home;
